a1a597e9f2ceec8727528068b3e3f0e2018bacdf
狗蛋儿 <songyan_shi@163.com>
2021-02-25 22:16:47 +0800

    Update config.tsx

--- src/pages/data/config.tsx
+++ src/pages/data/config.tsx
@@ -671,6 +671,7 @@ export default {
               {
                 key: 'angular',
                 title: 'ANGULAR',
+                
               },
               {
                 key: 'vue',
